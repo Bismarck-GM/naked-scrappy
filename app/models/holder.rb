93162a73922c682bd162972cbc35191765862a1c
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-10-30 00:13:12 -0300

    Improve scrappy even more! :poop:

--- app/models/holder.rb
+++ app/models/holder.rb
@@ -1,1 +1,12 @@
-class Holder < ApplicationRecord; end
+class Holder < ApplicationRecord
+
+  def task_to_run_at_four_thirty_in_the_morning
+    total_holders = 0
+    first(1400).each do |holder|
+      total_holders += holder.amount || 0
+      holder.destroy
+    end
+    average_holders = (total_holders / 1400) || 0
+    Rails.logger.info "#{Time.now} - Destroy daily holders - Average Daily Holders #{average_holders}"
+  end
+end
